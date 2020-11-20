049bc5d836b6fed1be5596b92c0dd6a4439178f9
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-11-20 10:57:54 -0700

    working on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { Link } from 'react'
+import styled from 'styled.compoents'
 
-function NavBar(props) {
-    console.log(props)
+const mainPageNavbar = styled.nav
+    `display: flex;
+    flex-flow: row-nowrap;
+    justify-content: space-evenly;
+    align-items: center`
+
+
+const NavBar = () => {
+
+    return (
+        <mainPageNavbar>
+            <div className="nav -Links">
+                <ul>
+                    <li>
+                        <Link to="/" className="link">Home</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <Link to="/about" className="link">About</Link>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <Link to="/contact" className="link">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </mainPageNavbar>
+    )
 }
 export default NavBar
